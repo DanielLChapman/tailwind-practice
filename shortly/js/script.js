48f533c6a3ca67de3df0cb7c90671bd0bda9ac3a
DanielLChapman <danielc@my.ccsu.edu>
2023-03-15 15:58:27 -0400

    finished app

--- shortly/js/script.js
+++ shortly/js/script.js
@@ -37,3 +37,14 @@ function validURL(str) {
   )
   return !!pattern.test(str)
 }
+
+const btn = document.getElementById('menu-btn')
+const menu = document.getElementById('menu')
+
+btn.addEventListener('click', navToggle)
+
+function navToggle() {
+  btn.classList.toggle('open')
+  menu.classList.toggle('flex')
+  menu.classList.toggle('hidden')
+}
