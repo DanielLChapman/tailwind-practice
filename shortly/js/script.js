43b8330761c55680ea8e41747ced8f4906785040
DanielLChapman <danielc@my.ccsu.edu>
2023-03-15 15:11:05 -0400

    Hero, form, and form validation

---
+++ shortly/js/script.js
@@ -0,0 +1,39 @@
+const input = document.getElementById('link-input');
+const linkForm = document.getElementById('link-form');
+const errMsg = document.getElementById('error-message');
+
+linkForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  let val = input.value;
+  if (!val) {
+    errMsg.innerHTML = 'Please enter something'
+    input.classList.add('border-red');
+    return;
+  }
+  if (!validURL(val)) {
+    errMsg.innerHTML = 'Invalid URL'
+    input.classList.add('border-red');
+    return;
+  }
+
+  errMsg.innerHTML = '';
+  input.classList.remove('border-red');
+  alert('Success')
+
+})
+
+
+// Validate a URL
+function validURL(str) {
+  var pattern = new RegExp(
+    '^(https?:\\/\\/)?' + // protocol
+      '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' +
+      '((\\d{1,3}\\.){3}\\d{1,3}))' +
+      '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' +
+      '(\\?[;&a-z\\d%_.~+=-]*)?' +
+      '(\\#[-a-z\\d_]*)?$',
+    'i'
+  )
+  return !!pattern.test(str)
+}
