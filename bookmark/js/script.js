355da824e1bf451ca82907b0ca1492b347867922
DanielLChapman <danielc@my.ccsu.edu>
2023-03-17 16:25:32 -0400

    panel js

--- bookmark/js/script.js
+++ bookmark/js/script.js
@@ -1,8 +1,38 @@
+const tabs = document.querySelectorAll('.tab');
+const panels = document.querySelectorAll('.panel');
+
+//tabs event listener
+tabs.forEach((x) => {
+  x.addEventListener('click', onTabClick)
+});
+
+function onTabClick(e) {
+  //deactivate all tabs
+  tabs.forEach((tab) => {
+    tab.children[0].classList.remove('border-softRed', 'border-b-4', 'md:border-b-0')
+  })
+
+  //hide all panels 
+  panels.forEach((panel) => {
+    panel.classList.add('hidden');
+  })
+
+  //activate a new tab
+  e.target.classList.add('border-softRed', 'border-b-4')
+  //show panel
+  const target = e.target.getAttribute('data-target');
+  document.getElementById('panels').getElementsByClassName(target)[0].classList.remove('hidden')
+}
+
+
+
+
+// Hamburger button listener
+
 const btn = document.getElementById('menu-btn')
 const menu = document.getElementById('menu')
 const logo = document.getElementById('logo')
 
-// Hamburger button listener
 btn.addEventListener('click', navToggle)
 
 
